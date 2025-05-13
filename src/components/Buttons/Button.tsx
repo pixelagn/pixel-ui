c3ee6c3bc88fbef62581b7c08aae21761a33c33b
Abdulrahman Bahyan <d7mii.b@gmail.com>
2025-05-13 23:29:21 +0300

    add color prop to ButtonProps and use handle color funtion in Button component

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -1,14 +1,17 @@
+import handleColor from "../../handlers/HandleColor"
 import { ButtonProps } from "./Button.type"
 
 const Button = ({ 
     text, 
     onClick,
     className,
+    color = "primary"
 }: ButtonProps) => {
+
     return (
         <button 
             className={
-                `px-4 py-2 rounded-md bg-blue-500 text-white ${className}`
+                `px-4 py-2 rounded-md cursor-pointer transition-all duration-200 ${handleColor(color)} ${className}`
             } 
             onClick={onClick}
         >
