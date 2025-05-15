7bd0d4dd2656d321b8219e951941763d89617a11
Abdulrahman Bahyan <d7mii.b@gmail.com>
2025-05-15 22:08:25 +0300

    create a sizes and size type

---
+++ src/tokens/size.ts
@@ -0,0 +1,10 @@
+export const sizes = {
+    extraSmall: "text-xs",
+    small: "text-sm",
+    regular: "text-base",
+    medium: "text-lg",
+    large: "text-xl",
+    extraLarge: "text-2xl",
+};
+
+export type sizeType = "extraSmall" | "small" | "regular" | "medium" | "large" | "extraLarge";
