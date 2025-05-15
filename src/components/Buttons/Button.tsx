cf582de856513bffaed8750b991c26835a92aa9a
Abdulrahman Bahyan <d7mii.b@gmail.com>
2025-05-15 20:23:03 +0300

    add href and target props to ButtonProps and hsndle it in button component

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -7,23 +7,40 @@ const Button = ({
     className,
     color = "primary",
     size = "regular",
-    disabled = false
+    disabled = false,
+    href,
+    target
 }: ButtonProps) => {
+
+    const classes: string = `
+        px-4 py-2 m-2 rounded-md cursor-pointer transition-all duration-200
+        disabled:cursor-not-allowed w-max
+        flex items-center justify-center gap-1
+        ${handleColor(color)} ${handleSize(size)} ${className}
+    `;
+
+    if (href) {
+        return (
+            <a 
+                className={classes} 
+                href={href}
+                target={target}
+                onClick={onClick}
+            >
+                { children }
+            </a>
+        );
+    }
 
     return (
         <button 
-            className={`
-                px-4 py-2 m-2 rounded-md cursor-pointer transition-all duration-200
-                disabled:cursor-not-allowed
-                flex items-center justify-center gap-1
-                ${handleColor(color)} ${handleSize(size)} ${className}
-            `} 
+            className={classes} 
             onClick={onClick}
             disabled={disabled}
         >
             { children }
         </button>
-    )
+    );
 }
 
 export default Button
