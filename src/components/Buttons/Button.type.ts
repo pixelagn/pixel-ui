cd6592caf65b70c6a5c443e2302713c94dae9d18
Abdulrahman Bahyan <d7mii.b@gmail.com>
2025-05-15 21:51:06 +0300

    using colors and color type in components

--- src/components/Buttons/Button.type.ts
+++ src/components/Buttons/Button.type.ts
@@ -1,8 +1,10 @@
+import { colorType } from "../../tokens/colors";
+
 export interface ButtonProps {
     children?: React.ReactNode | string;
     onClick?: () => void;
     className?: string;
-    color?: "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark";
+    color?: colorType;
     size?: "small" | "regular" | "medium" | "large";
     disabled?: boolean;
     href?: string;
