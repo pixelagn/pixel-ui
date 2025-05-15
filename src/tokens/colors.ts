0f141a64d693e531f104f8823a7fe1901e133f0e
Abdulrahman Bahyan <d7mii.b@gmail.com>
2025-05-15 21:50:27 +0300

    create a colors and color type

---
+++ src/tokens/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+    primary: "blue",
+    secondary: "violet",
+    success: "emerald",
+    danger: "red",
+    warning: "yellow",
+    info: "indigo",
+    light: "neutral",
+    dark: "slate",
+};
+
+export type colorType = "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark";
+
+export default colors;
