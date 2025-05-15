c91ebcb8e84530f8a68046755e6fb124a6c1def2
Abdulrahman Bahyan <d7mii.b@gmail.com>
2025-05-15 22:09:49 +0300

    use size type in components, and remove handleSize function from button hanlder component

--- src/components/Buttons/Button.handler.ts
+++ src/components/Buttons/Button.handler.ts
@@ -1,25 +1,10 @@
 import { colors, colorType } from "../../tokens/colors";
 
-export const handleSize = (size: string) => {
-    switch (size) {
-        case "small":
-            return "text-xs";
-        case "regular":
-            return "text-sm";
-        case "medium":
-            return "text-base";
-        case "large":
-            return "text-xl font-medium";
-        default:
-            return "text-base";
-    }
-}
-
 export const handleColor = (color: colorType) => {
-    if (color === colors.dark)
+    if (color === 'dark')
         return `bg-${colors.dark}-800 hover:bg-${colors.dark}-600 text-white disabled:bg-${colors.dark}-600`;
-    else if (color === colors.light)
+    else if (color === "light")
         return `bg-${colors.light}-100 hover:bg-${colors.light}-300 text-${colors.light}-800 disabled:bg-${colors.light}-300`;
     else 
-        return `bg-${colors[color]}-500 hover:bg-${colors.primary}-700 text-white disabled:bg-${colors.primary}-300`;
+        return `bg-${colors[color]}-500 hover:bg-${colors[color]}-700 text-white disabled:bg-${colors[color]}-300`;
 }
--- src/components/Buttons/Button.type.ts
+++ src/components/Buttons/Button.type.ts
@@ -1,11 +1,12 @@
 import { colorType } from "../../tokens/colors";
+import { sizeType } from "../../tokens/size";
 
 export interface ButtonProps {
     children?: React.ReactNode | string;
     onClick?: () => void;
     className?: string;
     color?: colorType;
-    size?: "small" | "regular" | "medium" | "large";
+    size?: sizeType;
     disabled?: boolean;
     href?: string;
     target?: string;
