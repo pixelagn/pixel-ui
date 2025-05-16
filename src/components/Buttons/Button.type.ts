6f1848ae408ad0131be0d328a799678c70f5f7de
Abdulrahman Bahyan <d7mii.b@gmail.com>
2025-05-16 20:32:47 +0300

    extends BaseProps in type of components

--- src/components/Buttons/Button.type.ts
+++ src/components/Buttons/Button.type.ts
@@ -1,8 +1,6 @@
-export interface ButtonProps {
-    children?: React.ReactNode | string;
-    onClick?: () => void;
-    className?: string;
-    color?: "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark";
+import { BaseProps } from "../../tokens/BaseProps.type";
+
+interface ButtonProps extends BaseProps {
     size?: "small" | "regular" | "medium" | "large";
     disabled?: boolean;
     href?: string;
@@ -10,3 +8,5 @@ export interface ButtonProps {
     startIcon?: React.ReactNode;
     endIcon?: React.ReactNode;
 };
+
+export default ButtonProps;
